a4260db0c89e0cb7368dde612a5fb7802c15d5e6
Artem Paliutin <pal.artem@icloud.com>
2022-01-27 16:36:36 +0300

    final

--- Motion/ParticleSystem.swift
+++ Motion/ParticleSystem.swift
@@ -11,4 +11,22 @@ class ParticleSystem {
     let image = Image("spark")
     var particles = Set<Particle>()
     var center = UnitPoint.center
+    var hue = 0.0
+    
+    func update(date: TimeInterval) {
+        let deleteDate = date - 1
+        
+        for particle in particles {
+            if particle.creationDate < deleteDate {
+                particles.remove(particle)
+            }
+        }
+        
+        let newParticle = Particle(x: center.x, y: center.y, hue: hue)
+        particles.insert(newParticle)
+        hue += 0.01
+        if hue > 1 {
+            hue -= 1
+        }
+    }
 }
