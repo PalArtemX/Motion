52667f87951b4e4967fb961154ad6b02e92d66a0
Artem Paliutin <pal.artem@icloud.com>
2022-01-27 13:55:42 +0300

    Initial Commit

---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Artem Paliutin on 27.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
